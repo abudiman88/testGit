1fbfb61c63e23fdf1b8f04e9c74c0ac63fa246d7
abudiman88 <adrian.budiman@gmail.com>
2018-07-29 10:55:21 +1000

    added a space

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -15,6 +15,7 @@ namespace WebApplication1
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
